08e1326248ab8e202b88753c380965cda72354e7
progriro <progriro@gmail.com>
2020-02-08 18:36:47 +0900

    ReactAppを作成

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+      date: new Date()
+    };
+    this.countUp = this.countUp.bind(this);
+    this.countDown = this.countDown.bind(this);
+  }
+
+  componentDidMount = () => {
+    this.timerId = setInterval(() => this.tick(), 1000);
+  };
+
+  componentWillUnmount = () => {
+    clearInterval(this.timerId);
+  };
+
+  tick = () => {
+    this.setState({
+      date: new Date()
+    });
+  };
+
+  countUp = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  countDown = () => {
+    this.setState({ count: this.state.count - 1 });
+  };
+
+  render() {
+    const textCenter = {
+      textAlign: "center"
+    };
+
+    const flexBox = {
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center",
+      width: "50%",
+      margin: "0 auto"
+    };
+
+    return (
+      <React.Fragment>
+        <h1 style={textCenter}>CountApp</h1>
+        <div style={flexBox}>
+          <h2>{this.state.count}</h2>
+          <div>
+            <button onClick={this.countUp}>+1</button>
+            <button onClick={this.countDown}>-1</button>
+          </div>
+        </div>
+        <div style={flexBox}>
+          <h2>now time: {this.state.date.toLocaleTimeString()}</h2>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
